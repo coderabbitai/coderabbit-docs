0333f83b6bc5c78484fbd6f1e3c32d82e0d29225
Karan Singh <ksingh209@gmail.com>
2023-12-22 00:26:16 -0800

    Update footer links and images

--- src/theme/Footer/index.js
+++ src/theme/Footer/index.js
@@ -29,32 +29,56 @@ export default function FooterWrapper(props) {
                     </div>
                     <div className="justify-start items-start gap-[100px] flex">
                         <div className="w-[140px] flex-col justify-center items-start gap-1 inline-flex">
-                            <div className="self-stretch h-[37px] text-white text-[15px]">
+                            <a
+                                href="https://blog.coderabbit.ai/docs/introduction"
+                                className="self-stretch h-[37px] text-white text-[15px]"
+                            >
                                 Docs
-                            </div>
-                            <div className="self-stretch h-[37px] text-white text-[15px] font-medium">
+                            </a>
+                            <a
+                                href="https://blog.coderabbit.ai/blog"
+                                className="self-stretch h-[37px] text-white text-[15px] font-medium"
+                            >
                                 Blog
-                            </div>
-                            <div className="self-stretch h-[37px] text-white text-[15px] font-medium">
+                            </a>
+                            <a
+                                href="https://coderabbit.ai/pricing"
+                                className="self-stretch h-[37px] text-white text-[15px] font-medium"
+                            >
                                 Pricing
-                            </div>
-                            <div className="self-stretch h-[37px] text-white text-[15px] font-medium">
+                            </a>
+                            <a
+                                href="#"
+                                className="self-stretch h-[37px] text-white text-[15px] font-medium"
+                            >
                                 Changelog
-                            </div>
+                            </a>
                         </div>
                         <div className="w-[134px] flex-col justify-center items-start gap-2 inline-flex">
-                            <div className="self-stretch h-[37px] text-white text-[15px] font-medium">
-                                Compliance{" "}
-                            </div>
-                            <div className="self-stretch h-[37px] text-white text-[15px] font-medium">
+                            <a
+                                href="YOUR_COMPLIANCE_LINK_HERE"
+                                className="self-stretch h-[37px] text-white text-[15px] font-medium"
+                            >
+                                Compliance
+                            </a>
+                            <a
+                                href="https://calendly.com/coderabbitai/30min"
+                                className="self-stretch h-[37px] text-white text-[15px] font-medium"
+                            >
                                 Schedule a Demo
-                            </div>
-                            <div className="self-stretch h-[37px] text-white text-[15px] font-medium text-nowrap">
+                            </a>
+                            <a
+                                href="https://coderabbit.ai/terms-and-conditions"
+                                className="self-stretch h-[37px] text-white text-[15px] font-medium text-nowrap"
+                            >
                                 Terms & Conditions
-                            </div>
-                            <div className="self-stretch h-[37px] text-white text-[15px] font-medium">
+                            </a>
+                            <a
+                                href="https://coderabbit.ai/privacy-policy"
+                                className="self-stretch h-[37px] text-white text-[15px] font-medium"
+                            >
                                 Privacy Policy
-                            </div>
+                            </a>
                         </div>
                     </div>
                 </div>
@@ -67,24 +91,42 @@ export default function FooterWrapper(props) {
                             CodeRabbit © 2023.
                         </div>
                         <div className="justify-end items-stretch self-stretch flex gap-4 pl-20 max-md:max-w-full max-md:flex-wrap max-md:pl-5">
-                            <img
-                                loading="lazy"
-                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/0f1b48f4977dc728d0cea441017f89664834046c1ce8f3916564bb9a1538f58a?apiKey=bcdfb569bac3439288cb09416e1bbb71&"
-                                className="aspect-square object-contain object-center w-5 overflow-hidden shrink-0 max-w-full"
-                                alt="image 1"
-                            />
-                            <img
-                                loading="lazy"
-                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/9ef98483828bc1c5e3349eb7c8da5c661ce0e7958e4dfe0e9c67db18e2019c65?apiKey=bcdfb569bac3439288cb09416e1bbb71&"
-                                className="aspect-square object-contain object-center w-5 overflow-hidden shrink-0 max-w-full"
-                                alt="image 2"
-                            />
-                            <img
-                                loading="lazy"
-                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/54cf490c6e905acb0ac1e2d5b9946ca1adae440948393edc7a12ec1a68b7b95c?apiKey=bcdfb569bac3439288cb09416e1bbb71&"
-                                className="aspect-square object-contain object-center w-5 overflow-hidden shrink-0 max-w-full"
-                                alt="image 3"
-                            />
+                            <a
+                                href="https://twitter.com/coderabbitai"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img
+                                    loading="lazy"
+                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/0f1b48f4977dc728d0cea441017f89664834046c1ce8f3916564bb9a1538f58a?apiKey=bcdfb569bac3439288cb09416e1bbb71&"
+                                    className="aspect-square object-contain object-center w-5 overflow-hidden shrink-0 max-w-full"
+                                    alt="image 1"
+                                />
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/company/coderabbitai"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img
+                                    loading="lazy"
+                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/9ef98483828bc1c5e3349eb7c8da5c661ce0e7958e4dfe0e9c67db18e2019c65?apiKey=bcdfb569bac3439288cb09416e1bbb71&"
+                                    className="aspect-square object-contain object-center w-5 overflow-hidden shrink-0 max-w-full"
+                                    alt="image 2"
+                                />
+                            </a>
+                            <a
+                                href="https://discord.gg/CVtemB5c"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                <img
+                                    loading="lazy"
+                                    src="https://cdn.builder.io/api/v1/image/assets/TEMP/54cf490c6e905acb0ac1e2d5b9946ca1adae440948393edc7a12ec1a68b7b95c?apiKey=bcdfb569bac3439288cb09416e1bbb71&"
+                                    className="aspect-square object-contain object-center w-5 overflow-hidden shrink-0 max-w-full"
+                                    alt="image 3"
+                                />
+                            </a>
                         </div>
                     </footer>
                 </div>
