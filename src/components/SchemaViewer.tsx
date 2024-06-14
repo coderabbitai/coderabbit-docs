9ff338699a6527d66d8091eda6a2af2587db124b
Hasit Mistry <hasit@coderabbit.ai>
2024-06-14 08:36:35 -0700

    Show CodeRabbit config schema from a file

--- src/components/SchemaViewer.tsx
+++ src/components/SchemaViewer.tsx
@@ -1,19 +1,6 @@
+import Schema from "@site/static/schema/schema.v2.json";
 import JSONSchemaViewer from "@theme/JSONSchemaViewer";
-import React from "react";
 
 export default function Viewer(): JSX.Element {
-  const [schema, setSchema] = React.useState(
-    undefined as undefined | Error | unknown
-  );
-
-  React.useEffect(() => {
-    fetch(
-      "https://storage.googleapis.com/coderabbit_public_assets/schema.v2.json"
-    )
-      .then((response) => response.json())
-      .then((data) => setSchema(data.definitions.schema))
-      .catch((err) => setSchema(err));
-  }, [schema]);
-
-  return <JSONSchemaViewer schema={schema} />;
+  return <JSONSchemaViewer schema={Schema.definitions.schema} />;
 }
